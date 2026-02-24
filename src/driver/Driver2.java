5114abecbc404d7eca097c5db6a52620634e1641
Grasiasimanullang <grasiagayatrisimanullang@gmail.com>
2026-02-24 13:42:11 +0700

    Update Driver2.java

--- src/driver/Driver2.java
+++ src/driver/Driver2.java
@@ -1,3 +1,37 @@
-public class Driver2 {
-    
+import java.util.Scanner;
+
+public class Driver1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 1. Input jumlah total data
+        int N = sc.nextInt();
+
+        int[] nilai = new int[N];
+
+        // 2. Input deret nilai
+        for (int i = 0; i < N; i++) {
+            nilai[i] = sc.nextInt();
+        }
+
+        // 3. Input kode kelompok (1 atau 2)
+        int kodeKelompok = sc.nextInt();
+
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            // Pola sesuai gambar (zig-zag / selang-seling)
+            if (kodeKelompok == 1 && i % 2 == 0) {
+                total += nilai[i];
+            } 
+            else if (kodeKelompok == 2 && i % 2 != 0) {
+                total += nilai[i];
+            }
+        }
+
+        System.out.println(total);
+
+        sc.close();
+    }
 }
